74051f6b73ccabd94a0909cd388c052f41797c00
Thailson <thailsonbezerra@outlook.com>
2023-12-12 14:45:39 -0300

    feat: buscando feriados municipais por uf

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import * as puppeteer from 'puppeteer';
+import delay from './utils/delay.js';
 
 (async () => {
   const browser = await puppeteer.launch({headless: false});
@@ -9,5 +10,23 @@ import * as puppeteer from 'puppeteer';
   await page.setViewport({width: 1080, height: 1024});
   await page.waitForSelector('.botao');
 
+  const ufList = await page.$$eval('#Uf > option', options => {
+    return options.map(option => option.textContent);
+  });
+
+  console.log(ufList)
+
+  for (let uf of ufList) {
+    if(uf === 'UF') continue;
+
+    await page.select('#Uf', uf);
+  }
+
+  await page.click('.botao');
+
+  await delay(10000);
+
   await browser.close();
+
+  process.exit()
 })();
